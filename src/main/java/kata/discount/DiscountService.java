16401207ad6297dfc173eefa09ae18ca37d72181
nick prendergast <nick_prendergast@hotmail.com>
2020-07-17 10:00:28 +0100

    make a hashmap data store for the discounts active

---
+++ src/main/java/kata/discount/DiscountService.java
@@ -0,0 +1,12 @@
+package kata.discount;
+
+public class DiscountService {
+
+    private final DiscountsChecker discountChecker;
+
+    public DiscountService(DiscountsChecker discountChecker) {
+        this.discountChecker = discountChecker;
+    }
+
+
+}
---
+++ src/main/java/kata/discount/DiscountsChecker.java
@@ -0,0 +1,51 @@
+package kata.discount;
+
+import kata.supermarket.DiscountType;
+import kata.supermarket.Item;
+import kata.supermarket.ProductType;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public final class DiscountsChecker {
+
+    private final Map<Item, DiscountType> discountOnIndividualProducts;
+    private final Map<ProductType, DiscountType> discountsOnProductType;
+
+    public DiscountsChecker() {
+        this.discountOnIndividualProducts = new HashMap<>();
+        this.discountsOnProductType = new HashMap<>();
+    }
+
+    public boolean productIdOrTypeInDiscountList(Item item, DiscountType discountType) {
+        return getDiscountOnIndividualProducts().get(item) == discountType
+                || getDiscountsOnProductType().get(item.productType()) == discountType;
+    }
+
+    public void addDiscountToIndividualProduct(Item item, DiscountType discountType) {
+        discountOnIndividualProducts.put(item, discountType);
+    }
+
+    public void addDiscountToProductType(ProductType productType, DiscountType discountType) {
+        discountsOnProductType.put(productType, discountType);
+    }
+
+    public Set<DiscountType> getCurrentDiscountTypesOnItems() {
+        Set<DiscountType> activeDiscountTypes = new HashSet<>();
+        activeDiscountTypes.addAll(discountOnIndividualProducts.values());
+        activeDiscountTypes.addAll(discountsOnProductType.values());
+        return activeDiscountTypes;
+    }
+
+    private Map<Item, DiscountType> getDiscountOnIndividualProducts() {
+        return discountOnIndividualProducts;
+    }
+
+    private Map<ProductType, DiscountType> getDiscountsOnProductType() {
+        return discountsOnProductType;
+    }
+
+
+}
